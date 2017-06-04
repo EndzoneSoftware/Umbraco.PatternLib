3dfc13a923a48ffdf5e0a7630453542e9bc44a39
Barry <barry@endzone.co.uk>
2017-06-04 12:59:07 +0100

    Pattern nav structure in viewer & some sample patterns

--- Core/Controllers/PatternLibController.cs
+++ Core/Controllers/PatternLibController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Endzone.Umbraco.PatternLib.Core.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ namespace Endzone.Umbraco.PatternLib.Core.Controllers
     public class PatternLibController : Controller
     {
         const string viewTemplatePath = "~/Views/_patternlib/viewer/{0}.cshtml";
+        const string staticPatternsPath = "~/Views/_patternlib/patterns/";
 
         string ViewTemplate(string name)
         {
@@ -28,7 +31,30 @@ namespace Endzone.Umbraco.PatternLib.Core.Controllers
         [ChildActionOnly]
         public ActionResult PatternNav()
         {
-            return View(ViewTemplate("_patternNavBar"));
+            //todo ensure that these dirs exist
+            var path = Server.MapPath(staticPatternsPath);
+            var patterns = new PatternLibrary();
+            foreach (var d in Directory.EnumerateDirectories(path))
+            {
+                var patternDir = new Pattern(d);
+                GetPatternsFromDir(d, patternDir);
+                patterns.Add(patternDir);
+            }
+            return View(ViewTemplate("_patternNavBar"), patterns);
+        }
+
+        private static void GetPatternsFromDir(string d, Pattern patternDir)
+        {
+            foreach (var f in Directory.EnumerateFiles(d, "*.htm", SearchOption.TopDirectoryOnly))
+            {
+                patternDir.Add(new Pattern(f));
+            }
+            foreach (var subDir in Directory.EnumerateDirectories(d))
+            {
+                var pattern = new Pattern(subDir);
+                GetPatternsFromDir(subDir, pattern);
+                patternDir.Add(pattern);
+            }
         }
     }
 }
--- Core/Models/Pattern.cs
+++ Core/Models/Pattern.cs
@@ -1,22 +1,50 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Endzone.Umbraco.PatternLib.Core.Models
 {
+    public class PatternLibrary : List<Pattern>
+    {
+
+    }
     public class Pattern
     {
-        FileInfo file;
+        /// <summary>
+        /// The full path to the directory or file
+        /// </summary>
+        public string FullPath { get; }
+
+        public bool IsList
+        {
+            get
+            {
+                return Path.GetExtension(this.FullPath) == String.Empty;
+            }
+        }
 
         public string Name
         {
             get
             {
-                return file.Name;
+                string fileName = Path.GetFileNameWithoutExtension(this.FullPath);
+                //lets ignore prefix numbers (used for ordering only)
+                return Regex.Replace(fileName, "^[0-9]{0,2}[-]{0,1}", "");
             }
         }
 
-        public Pattern(FileInfo file)
+        public List<Pattern> Patterns { get; }
+
+        public Pattern(string path)
         {
-            this.file = file;
+            this.FullPath = path;
+            Patterns = new List<Pattern>();
+        }
+
+        public void Add(Pattern p)
+        {
+            Patterns.Add(p);
         }
     }
 }
