80ece358888b14680c35bc0af64961ea6bad3d96
Barry <barry@endzone.co.uk>
2017-06-17 22:58:29 +0100

    Move App_Plugins files to package project & add post build script to copy to test site

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Endzone.io")]
 [assembly: AssemblyProduct("Endzone.Umbraco.PatternLib")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
